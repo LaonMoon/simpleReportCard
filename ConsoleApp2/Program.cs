4e43ac1b0024caec73e4c0acd4d404e9b02b8f9b
LaonMoon <laonmun@gmail.com>
2019-12-29 15:02:57 +0900

    4.5th

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,35 +8,33 @@ namespace ConsoleApp2
         {
             Console.WriteLine("학생 수를 입력해 주세요.");
             int studentcount = int.Parse(Console.ReadLine());
-            
+            string[] name = new string[studentcount];                
             int[] korean = new int[studentcount];
             int[] english = new int[studentcount];
             int[] math = new int[studentcount];
 
-            int number = 0;
-            while (number<studentcount)
+            for (int number = 0;number<studentcount;number++)
             {
                 Console.Write(number);
-                Console.WriteLine("번의 점수를 입력하세요.");
+                Console.WriteLine("번의 이름을 입력하세요.");
+                name[number] = Console.ReadLine();
                 Console.WriteLine("국어 점수를 입력하세요.");
                 korean[number] = int.Parse(Console.ReadLine());
                 Console.WriteLine("영어 점수를 입력하세요.");
                 english[number] = int.Parse(Console.ReadLine());
                 Console.WriteLine("수학 점수를 입력하세요.");
                 math[number] = int.Parse(Console.ReadLine());
-                number = number + 1;
             }
-            number = 0;
-            while (number<studentcount)
+
+            for (int number = 0; number < studentcount; number++)
             {
-                Console.Write(number);
-                Console.Write("번의 총점은 ");
+                Console.Write(name[number]);
+                Console.Write("의 총점은 ");
                 Console.Write(korean[number] + english[number] + math[number]);
                 Console.WriteLine("점 입니다.");
                 Console.Write("평균 점수는 ");
                 Console.Write((korean[number] + english[number] + math[number]) / 3);
                 Console.WriteLine("점 입니다.");
-                number = number + 1;
             }
         }
     }
